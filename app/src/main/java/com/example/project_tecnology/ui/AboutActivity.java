feee06a9f62eec221df9a6239f1aa5c1d360975e
Zul <m.zulasfi17@gmail.com>
2024-06-11 22:14:53 +0700

    update about

--- app/src/main/java/com/example/project_tecnology/ui/AboutActivity.java
+++ app/src/main/java/com/example/project_tecnology/ui/AboutActivity.java
@@ -2,15 +2,29 @@ package com.example.project_tecnology.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.project_tecnology.MainActivity;
 import com.example.project_tecnology.R;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private ImageView IVBackProfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        IVBackProfil = findViewById(R.id.IVBackProfil);
+
+
+        IVBackProfil.setOnClickListener(v -> {
+            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
